0985bec5d6eac21338817a631a989bd966adbeed
mxyffff <rua0254@naver.com>
2026-04-21 14:26:33 +0900

    init: first commit

---
+++ src/main/java/com/routinetown/on/OnApplication.java
@@ -0,0 +1,13 @@
+package com.routinetown.on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnApplication.class, args);
+	}
+
+}
